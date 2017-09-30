76143e87562c9830a5069d2bd0de448fd239830f
Caryl Stuart <carylstuart77@gmail.com>
2017-09-30 17:06:26 -0600

    Converting Gallons to Liters with improved code.

--- src/edu/cnm/deepdive/prework/GalToLit.java
+++ src/edu/cnm/deepdive/prework/GalToLit.java
@@ -8,12 +8,20 @@ public class GalToLit {
   public static void main(String args[]) {
     double gallons; //holds the number of gallons
     double liters;  //holds conversion to liters
+    int counter;
 
-    gallons = 12; //Number of gallons to convert
+    counter = 0;
+    for (gallons = 1; gallons <= 100; gallons++) {
+      liters = gallons * 3.7854;  //3.7854 litters in a gallon; convert to litters
 
-    liters = gallons * 3.7854;  //3.7854 litters in a gallon; convert to litters
-
-    System.out.println(gallons + " gallons is " + liters + " liters.");
+      System.out.println(gallons + " gallons is " + liters + " liters.");
+      counter++;
+      //every 10th line, print a blank line
+      if (counter == 10) {
+        System.out.println();
+        counter = 0; //reset the line counter.
+      }
+    }
 
   }
 
